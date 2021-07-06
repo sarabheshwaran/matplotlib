0a42169bbc3d73c7321e2ed024efefbc38b35225
sarabheshwaran <81975881+sarabheshwaran@users.noreply.github.com>
2021-07-06 19:26:43 +0530

    Add files via upload

---
+++ scatter_points.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.scatter(2 , 4 , s=100)
+
+plt.title("DOt", fontsize=13)
+plt.xlabel("vdgal", fontsize=13)
+plt.ylabel("sqval", fontsize=13)
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
